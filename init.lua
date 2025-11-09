b87f1f47dd9671d500f5be38ac8353122ff8c924
Ruben <rsm.ruben@gmail.com>
2025-11-09 20:44:20 +0100

    Configure dracula theme. Add relative numbers

--- init.lua
+++ init.lua
@@ -4,16 +4,28 @@ vim.cmd([[
 
   " List your plugins here, for example:
   " Plug 'tpope/vim-sensible'
+  Plug 'dracula/vim', { 'as': 'dracula' }
   Plug 'nvim-lua/plenary.nvim'
   Plug 'nvim-telescope/telescope.nvim'
 
   call plug#end()
 ]])
 
+-- Use comma a leader key
 vim.g.mapleader = ","
 
+-- Configure Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Configure Dracula as theme
+vim.cmd('colorscheme dracula')
+
+-- Set relative numbers
+vim.opt.relativenumber = true
+
+-- Show absolute line number for current line
+vim.opt.number = true
