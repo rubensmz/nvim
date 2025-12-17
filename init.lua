60284b14921b848d9d0aec65ca34d7e2f8391b1e
rubensmz <rsm.ruben@gmail.com>
2025-12-17 10:13:21 +0100

    Update init.lua
    Replace Plug with Lazy

--- init.lua
+++ init.lua
@@ -1,50 +1,78 @@
+
 -- Set relative numbers
 vim.opt.relativenumber = true
 -- Show absolute line number for current line
 vim.opt.number = true
 -- Use spaces instead of <Tab>
-vim.opt.expandtab   = true  
+vim.opt.expandtab = true
 -- Visual width of a tab character
-vim.opt.tabstop     = 3     
+vim.opt.tabstop = 3
 -- Indent size for >>, <<, autoindent, etc.
-vim.opt.shiftwidth  = 3     
+vim.opt.shiftwidth = 3
 -- How many spaces <Tab> inserts in insert mode
-vim.opt.softtabstop = 3     
--- Use comma a leader key
+vim.opt.softtabstop = 3
+
+-- Use comma as leader key
 vim.g.mapleader = ","
+
 -- Normal mode: Space starts a forward search
 vim.keymap.set("n", "<Space>", "/", { noremap = true, silent = false })
+
 -- Case-insensitive by default...
 vim.o.ignorecase = true
 -- ...but if the search pattern has any uppercase letter, make it case-sensitive
-vim.o.smartcase  = true
+vim.o.smartcase = true
 
 if vim.g.vscode then
-	-- Running inside VS Code
-	-- VSCode-specific config here
+   -- Running inside VS Code
+   -- VSCode-specific config here
 else
-	-- Plain NeoVim
-	-- Define the Vimscript command to initialize vim-plug
-	vim.cmd([[
-	  call plug#begin('~/.local/share/nvim/plugged')
+   -- Plain Neovim
 
-	  " List your plugins here, for example:
-	  " Plug 'tpope/vim-sensible'
-	  Plug 'dracula/vim', { 'as': 'dracula' }
-	  Plug 'nvim-lua/plenary.nvim'
-	  Plug 'nvim-telescope/telescope.nvim'
-      Plug 'nfvs/vim-perforce'
-	  call plug#end()
-	]])
+   -- Bootstrap lazy.nvim
+   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+   local uv = vim.uv or vim.loop
+   if not uv.fs_stat(lazypath) then
+      vim.fn.system({
+         "git",
+         "clone",
+         "--filter=blob:none",
+         "https://github.com/folke/lazy.nvim.git",
+         "--branch=stable",
+         lazypath,
+      })
+   end
+   vim.opt.rtp:prepend(lazypath)
 
-	-- Configure Telescope
-	local builtin = require('telescope.builtin')
-	vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-	vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-	vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-	vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+   -- Plugins
+   require("lazy").setup({
+      -- Telescope + dependency
+      {
+         "nvim-telescope/telescope.nvim",
+         dependencies = { "nvim-lua/plenary.nvim" },
+      },
+      -- Perforce
+      { "nfvs/vim-perforce" },
+      -- Theme (load early, don't lazy-load)
+      {
+         "catppuccin/nvim",
+         name = "catppuccin",
+         lazy = false,
+         priority = 1000,
+         config = function()
+            vim.cmd.colorscheme("catppuccin-mocha")
+         end,
+      },
+   }) -- plugin spec format and setup are per lazy.nvim docs [web:1][web:51]
 
-	-- Configure Dracula as theme
-	vim.cmd('colorscheme dracula')
+   require("catppuccin").setup({
+      auto_integrations = true,
+   })
 
+   -- Configure Telescope (safe to require after lazy.setup)
+   local builtin = require("telescope.builtin")
+   vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+   vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Telescope live grep" })
+   vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Telescope buffers" })
+   vim.keymap.set("n", "<leader>fh", builtin.help_tags,  { desc = "Telescope help tags" })
 end
