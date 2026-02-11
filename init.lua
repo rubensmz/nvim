81cf863bae05ce84518a457b0cf3576f0fb95a88
rubensmz <rsm.ruben@gmail.com>
2026-02-11 16:59:35 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 -- Better colors & always show tabline (bufferline lives here)
 vim.opt.termguicolors = true
 vim.opt.showtabline = 2
@@ -26,6 +25,14 @@ vim.o.smartcase = true
 -- Clear search highlight
 vim.keymap.set("n", "<leader><CR>", "<cmd>nohlsearch<CR>", { silent = true, desc = "Clear search highlight" })
 
+-- Filetype detection for SystemVerilog/Verilog
+vim.filetype.add({
+   extension = {
+      v = 'verilog',
+      sv = 'systemverilog',
+      svh = 'systemverilog',
+   },
+})
 
 if vim.g.vscode then
    -- Running inside VS Code
@@ -50,6 +57,7 @@ else
 
    -- Plugins
    require("lazy").setup({
+
       -- Telescope + dependency
       {
          "nvim-telescope/telescope.nvim",
@@ -72,62 +80,127 @@ else
 
       -- File explorer
       {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        lazy = false,
-        dependencies = {
-          "nvim-tree/nvim-web-devicons", -- icons
-        },
-        config = function()
-          require("nvim-tree").setup({})
-        end,
+         "nvim-tree/nvim-tree.lua",
+         version = "*",
+         lazy = false,
+         dependencies = {
+            "nvim-tree/nvim-web-devicons", -- icons
+         },
+         config = function()
+            require("nvim-tree").setup({
+               on_attach = function(bufnr)
+                  local api = require("nvim-tree.api")
+
+                  -- Load all default keybindings first
+                  api.config.mappings.default_on_attach(bufnr)
+                  -- Use + to change root to the directory under cursor
+                  vim.keymap.set('n', '+', api.tree.change_root_to_node, {
+                     buffer = bufnr,
+                     desc = 'Change root to node'
+                  })
+               end,
+
+            })
+         end,
       },
 
       -- Statusline
       {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional icons
-        config = function()
-          require("lualine").setup({
-            options = {
-              theme = "catppuccin",
-              icons_enabled = true,
-              globalstatus = true, -- single statusline across splits (nvim >= 0.7)
+         "nvim-lualine/lualine.nvim",
+         dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional icons
+         config = function()
+            require("lualine").setup({
+               options = {
+                  theme = "catppuccin",
+                  icons_enabled = true,
+                  globalstatus = true, -- single statusline across splits (nvim >= 0.7)
 
-              section_separators = { left = "", right = "" },
-              component_separators = "",
-            },
-          })
-        end,
-        extensions = { "quickfix", "nvim-tree", "toggleterm", "lazy" },
+                  section_separators = { left = "", right = "" },
+                  component_separators = "",
+               },
+            })
+         end,
+         extensions = { "quickfix", "nvim-tree", "toggleterm", "lazy" },
       },
 
       -- Bufferline: show buffers in the tabline (top bar)
       {
-        "akinsho/bufferline.nvim",
-        version = "*",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-          require("bufferline").setup({
-            options = {
-              mode = "buffers",                 -- show buffers, not tabs
-              always_show_bufferline = true,
-              show_buffer_close_icons = false,
-              show_close_icon = false,
-              separator_style = "slant",        -- "slant" | "thin" | "padded_slant" | etc.
-              diagnostics = false,              -- set to "nvim_lsp" if you want LSP diagnostics
-              offsets = {
-                { filetype = "NvimTree", text = "Explorer", text_align = "left" },
-              },
-            },
-          })
+         "akinsho/bufferline.nvim",
+         version = "*",
+         dependencies = { "nvim-tree/nvim-web-devicons" },
+         config = function()
+            require("bufferline").setup({
+               options = {
+                  mode = "buffers",                 -- show buffers, not tabs
+                  always_show_bufferline = true,
+                  show_buffer_close_icons = false,
+                  show_close_icon = false,
+                  separator_style = "slant",        -- "slant" | "thin" | "padded_slant" | etc.
+                  diagnostics = "nvim_lsp",         -- show LSP diagnostics in bufferline
+                  offsets = {
+                     { filetype = "NvimTree", text = "Explorer", text_align = "left" },
+                  },
+               },
+            })
 
-          -- Keymaps: cycle buffers with Tab / Shift-Tab
-          vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { silent = true, desc = "Next buffer" })
-          vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { silent = true, desc = "Prev buffer" })
-        end,
+            -- Keymaps: cycle buffers with Tab / Shift-Tab
+            vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { silent = true, desc = "Next buffer" })
+            vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { silent = true, desc = "Prev buffer" })
+         end,
       },
-   }) -- plugin spec format and setup are per lazy.nvim docs [web:1][web:51]
+
+      -- LSP Configuration
+      {
+         "neovim/nvim-lspconfig",
+         event = { "BufReadPre", "BufNewFile" },
+         config = function()
+            -- Configure Verible LSP using the new vim.lsp.config API
+            vim.lsp.config.verible = {
+               cmd = {'verible-verilog-ls', '--rules_config_search'},
+               root_markers = {'.git'},
+               filetypes = {'verilog', 'systemverilog'},
+            }
+
+            -- Enable Verible LSP
+            vim.lsp.enable('verible')
+
+            -- Set up LSP keybindings using LspAttach autocmd
+            vim.api.nvim_create_autocmd('LspAttach', {
+               callback = function(args)
+                  local bufnr = args.buf
+                  local opts = { buffer = bufnr, noremap = true, silent = true }
+
+                  -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                  -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                  -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                  -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                  -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+                  -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                  -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                  -- vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format({ async = true }) end, opts)
+               end,
+            })
+
+            -- Configure diagnostic display
+            vim.diagnostic.config({
+               virtual_text = true,
+               signs = true,
+               underline = true,
+               update_in_insert = false,
+               severity_sort = true,
+            })
+
+            -- Diagnostic signs in the gutter
+            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+            for type, icon in pairs(signs) do
+               local hl = "DiagnosticSign" .. type
+               vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+            end
+         end,
+      },
+
+
+   }) -- plugin spec format and setup are per lazy.nvim docs
 
    require("catppuccin").setup({
       auto_integrations = true,
@@ -139,7 +212,7 @@ else
 
    -- Choose what buffer close with <leader>bc
    vim.keymap.set("n", "<leader>bc", "<cmd>BufferLinePickClose<CR>", { desc = "Pick buffer to close" })
-   
+
    -- nvim-tree keymaps
    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
    vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFindFile<CR>", { desc = "Reveal current file in tree" })
@@ -150,4 +223,11 @@ else
    vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Telescope live grep" })
    vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Telescope buffers" })
    vim.keymap.set("n", "<leader>fh", builtin.help_tags,  { desc = "Telescope help tags" })
+
+   -- LSP keymaps (global, not buffer-specific)
+   vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = "Show diagnostics" })
+   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Diagnostics to loclist" })
 end
+
